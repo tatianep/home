6cd8d478899c5a250b79a52d4f36ca3920239e29
Tatiane Santos <tatianepereiradossantos@gmail.com>
2024-07-13 21:45:08 -0400

    Updated README.md to match current content. Deleted READMEdocs

--- READMEdocs/custom-deployment copy.md
+++
@@ -1,9 +0,0 @@
-## Personal Website 
-
-Website template originally from [Hashir Shoaeb](https://github.com/hashirshoaeb/home)
-
-## Modifications so far
-
-1. Added Publications
-2. Added Awards
-3. Changed router, from BrowserRoute to HashRoute
--- pages.js
+++ pages.js
@@ -1,6 +1,6 @@
 const ghpages = require("gh-pages");
 const pathname = `${__dirname}/build`;
-const repoURL = "https://github.com/tatianep/tatianesantos.github.io.git";
+const repoURL = "https://github.com/tatianep/home.git";
 
 ghpages.publish(
   pathname,
